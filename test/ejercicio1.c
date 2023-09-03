a73c52a48c7f1763046eab64e1d7a4076fd7eba2
DESKTOP-I0PJVK6\claud <agustina.cardellal@gmail.com>
2023-09-03 19:24:26 -0300

    aaaaaaaaaa

---
+++ test/ejercicio1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int count = 8;
+    for (int i = 0; i < count; i++)
+    {
+        printf("Hello Agustina!: %d\n", i)
+    }
+    
+    return 0;
+}
--- test/main.c
+++ test/main.c
@@ -3,11 +3,7 @@
 
 int main()
 {
-    int count = 8;
-    for (int i = 0; i < count; i++)
-    {
-        printf("Hello Agustina!: %d\n", i);
-    }
+    printf("Hello Agustina!\n")
     
     return 0;
 }
