a026cfc58fefbb95e05ec9be073e5193d364fcac
DESKTOP-I0PJVK6\claud <agustina.cardellal@gmail.com>
2023-09-23 19:24:01 -0300

    ejercicio vectores iguales

--- ejercicio7.1.4.c
+++ ejercicio7.1.4.c
@@ -5,12 +5,16 @@ int main()
 {
 
 int ce = 10;
-int v1[10] = {10, 5, 20, 8, 15, 12, 7, 3, 9, 11};
-int v2[10] = {10, 5, 20, 8, 15, 12, 7, 3, 9, 11};
+int igual_dist;
+int v1[10];
+//= {10, 5, 20, 8, 15, 12, 9, 3, 9, 11};
+int v2[10];
+//= {10, 5, 20, 8, 15, 12, 7, 3, 9, 11};
 
-carga(v1[], ce);
-carga(v2[], ce);
-igual_dist = vec_iguales(v1[], v2[], ce);
+//llamo a las funciones sin poner corchetes en los vectores
+carga(v1, ce);
+carga(v2, ce);
+igual_dist = vec_iguales(v1, v2, ce);
 
 if (igual_dist==1)
     {
@@ -22,9 +26,10 @@ else
     }
 }
 
+//declaro i en el for
 int vec_iguales (int v1[], int v2[], int ce)
 {
-    for(i=0 ; i<ce ; i++)
+    for(int i=0 ; i<ce ; i++)
     {
         if (v1[i]!=v2[i])
         {
@@ -34,11 +39,11 @@ int vec_iguales (int v1[], int v2[], int ce)
     return 1;
 }
 
-void carga (int v1[], int ce)
+void carga (int v[], int ce)
 {
-for (i=0 ; i < ce ; i++)
+for (int i=0 ; i < ce ; i++)
     {
     printf ("Ingrese un valor: ");
-    scanf ("%d", &v[]);
+    scanf ("%d", &v[i]);
     }
 }
