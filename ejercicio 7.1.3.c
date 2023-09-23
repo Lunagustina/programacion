9678e7a61b16679d051d5f486c8fe12473847aa3
DESKTOP-I0PJVK6\claud <agustina.cardellal@gmail.com>
2023-09-23 00:41:31 -0300

    hola

---
+++ ejercicio 7.1.3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+// declare functions
+
+void cargar (int v[], int ce);
+void intercalar (int v1[], int v2[], int v_inter[], int ce);
+void mostrar (int v[], int ce);
+
+//main
+
+int main()
+
+{
+int v1[5], v2[5], v_inter[10];
+cargar (v1,5);
+cargar (v2,5);
+intercalar (v1, v2, v_inter,5);
+mostrar (v_inter,10);
+
+return 0;
+}
+
+//function's definition
+
+void cargar (int v[], int ce)
+
+{
+    for (int i=0 ; i<ce ; i++)
+        {
+        printf ("Ingrese un valor");
+        scanf ("%d", &v[i]);
+        }
+}
+
+void intercalar (int v1[], int v2[], int v_inter[], int ce)
+{
+    for (int i=0 ; i<ce ; i++)
+        {
+        v_inter[2*i] = v1 [i];
+        v_inter [(2*i)+1] = v2 [i];
+        }
+}
+
+void mostrar (int v[], int ce)
+{
+printf ("Matriz intercalada: ");
+
+    for ( int i=0 ; i<ce ; i++)
+        {
+        printf ("%d", &v[i]);
+        }
+}
